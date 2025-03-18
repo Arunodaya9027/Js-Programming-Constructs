19bf3a0e14f61211be3789636378a763ab440c2a
Arunodaya9027 <arunodaya9027p.sg@gmail.com>
2025-03-18 22:37:50 +0530

    [Arunodaya Pratap Singh] Add. Added Solutions of Js If-Else if-Else Problems

--- If-ElseIf-Else Problems/DisplayWeekDay.js
+++ If-ElseIf-Else Problems/DisplayWeekDay.js
@@ -1,25 +1,25 @@
-let digit = parseInt(process.argv[2]);
+let weekDay = parseInt(process.argv[2]);
 
-if (digit == 1)
-    console.log("Sunday");
-
-else if (digit == 2)
+if (weekDay == 1)
     console.log("Monday");
 
-else if (digit == 3)
+else if (weekDay == 2)
     console.log("Tuesday");
 
-else if (digit == 4)
+else if (weekDay == 3)
     console.log("Wednesday");
 
-else if (digit == 5)
+else if (weekDay == 4)
     console.log("Thursday");
 
-else if (digit == 6)
+else if (weekDay == 5)
     console.log("Friday");
 
-else if (digit == 7)
+else if (weekDay == 6)
     console.log("Saturday");
+
+else if (weekDay == 7)
+    console.log("Sunday");
 
 else
     console.log("Invalid Input");
